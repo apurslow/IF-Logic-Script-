97a18027647113e69d0c1bd97a1881d24a128afd
apurslow <apurslow@users.noreply.github.com>
2022-07-08 14:58:06 +0000

    first

---
+++ logic.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import keyboard
+import time
+
+
+# player health counter default value
+playerLife = 3
+
+# something for the player to want
+gold = 0
+
+# current game state
+gameOver = False
+
+# keyboard directions
+keys = [
+    "down",
+    "up",
+    "left",
+    "right",
+    "w",
+    "s",
+    "a",
+    "d"
+]
+
+# list of directions
+playerMoves = ["return", "forward!", "left", "right"]
+
+# dictionary of places with answers
+rooms = {"Room1": ["orcs! lose 1 health",
+    "healing fountain! gain 1 health", "empty! choose next door"]}
+
+def chooseDirection(keypressed):
+    print("Key pressed = ")
+    try:
+        if keyboard.is_pressed("left") or keyboard.is_pressed("a"):
+                print("left!")
+        elif keyboard.is_pressed("right") or keyboard.is_pressed("d"):
+                print("right!")
+        elif keyboard.is_pressed("up") or keyboard.is_pressed("w"):
+                print("up!")
+        elif keyboard.is_pressed("down") or keyboard.is_pressed("s"):
+                print("return!")
+                gameOver = True
+    except:
+        print("wrong key pressed...")
+        print("please use w,a,s,d or up,left,down,right arrows for movement selection")
+        
+
+
+
+
+
+print("Game starting....")   
+keyboard.on_press(chooseDirection)
+
+
+
+    
